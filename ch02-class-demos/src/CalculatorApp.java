d8022acd164e12955b2788f77a51a6c79f97dd29
Sean Blessing <snblessing@gmail.com>
2021-06-02 15:28:05 -0400

    ch2 more class demose

--- ch02-class-demos/src/CalculatorApp.java
+++ ch02-class-demos/src/CalculatorApp.java
@@ -1,30 +1,41 @@
+import java.util.Scanner;
 
 public class CalculatorApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator app!");
-		
-		int n1 = 5;
-		int n2 = 7;
-		int sum = n1 + n2;
-		System.out.println("Sum = "+sum);
-		
-		int difference = n2 - n1;
-		System.out.println("Difference = "+difference);
-		
-		int product = n2 * n1;
-		System.out.println("Product = "+product);
-		
-		// quotient
-		System.out.println("Quotient = "+(n2/n1));
-		
-		int modulus = n2%n1;
-		System.out.println("Modulus = "+modulus);
-		
-		
 
+		// p. 55 - Scanner
+		Scanner sc = new Scanner(System.in);
+
+		// p. 69 while loop start
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.print("Enter a whole number: ");
+			int n1 = sc.nextInt();
+			System.out.print("Enter another whole number: ");
+			int n2 = sc.nextInt();
+			int sum = n1 + n2;
+			System.out.println("Sum = " + sum);
+
+			int difference = n2 - n1;
+			System.out.println("Difference = " + difference);
+
+			int product = n2 * n1;
+			System.out.println("Product = " + product);
+
+			// quotient
+			System.out.println("Quotient = " + (n2 / n1));
+
+			int modulus = n2 % n1;
+			System.out.println("Modulus = " + modulus);
+
+			// while loop end
+			System.out.print("Continue? (y/n)");
+			choice = sc.next();
+		}
 		System.out.println("Bye");
-
+		sc.close();
 	}
 
 }
--- ch02-class-demos/src/ClassDemoApp.java
+++ ch02-class-demos/src/ClassDemoApp.java
@@ -5,33 +5,52 @@ public class ClassDemoApp {
 		System.out.println("Welcome");
 		// p. 39
 		// single line comment
-		
+
 		/*
-		 * This is a
-		 * multi line comment
+		 * This is a multi line comment
 		 */
-		
-		//p. 45
+
+		// p. 45
 		int counter = 1;
 		double unitPrice = 14.99;
-		
+
 		int i1 = 5;
 		int i2 = 7;
-		
+
 		double d1 = 11.1;
 		double d2 = 25.444;
-		
+
 		counter = 2;
 		unitPrice = 13.99;
-		
+
 		d2 = d1;
 		d1 = 7.99;
 		System.out.println(d1);
 		System.out.println(d2);
+
+		// casting p. 46
+		int a = 5;
+		double b = 8.3;
+		// int sum = a + b; -> invalid
+		double sum2 = a + b;
+		System.out.println("sum2 = " + sum2);
+		int sum3 = a + (int) b;
+
+		// p. 47 increment a counter
+		counter = 1;
+		// several ways to increment an int
+		counter = counter + 1;
+		counter += 1;
+		counter++;
+		System.out.println("counter = "+counter);
 		
-		
-		
-		
+		// p. 48 Strings
+		String fName = "Bob";
+		String mName = "Nesta";
+		String lName = "Marley";
+		String fullName = fName + " " + mName + " " + lName;
+		System.out.println(fullName);
+
 		System.out.println("Bye");
 
 	}
