21eafac7d01bb52d412a2c3e223721fd04a0e792
Sean Blessing <snblessing@gmail.com>
2021-06-02 12:35:22 -0400

    ch2 demos

---
+++ ch02-class-demos/src/CalculatorApp.java
@@ -0,0 +1,30 @@
+
+public class CalculatorApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the calculator app!");
+		
+		int n1 = 5;
+		int n2 = 7;
+		int sum = n1 + n2;
+		System.out.println("Sum = "+sum);
+		
+		int difference = n2 - n1;
+		System.out.println("Difference = "+difference);
+		
+		int product = n2 * n1;
+		System.out.println("Product = "+product);
+		
+		// quotient
+		System.out.println("Quotient = "+(n2/n1));
+		
+		int modulus = n2%n1;
+		System.out.println("Modulus = "+modulus);
+		
+		
+
+		System.out.println("Bye");
+
+	}
+
+}
---
+++ ch02-class-demos/src/ClassDemoApp.java
@@ -0,0 +1,39 @@
+
+public class ClassDemoApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		// p. 39
+		// single line comment
+		
+		/*
+		 * This is a
+		 * multi line comment
+		 */
+		
+		//p. 45
+		int counter = 1;
+		double unitPrice = 14.99;
+		
+		int i1 = 5;
+		int i2 = 7;
+		
+		double d1 = 11.1;
+		double d2 = 25.444;
+		
+		counter = 2;
+		unitPrice = 13.99;
+		
+		d2 = d1;
+		d1 = 7.99;
+		System.out.println(d1);
+		System.out.println(d2);
+		
+		
+		
+		
+		System.out.println("Bye");
+
+	}
+
+}
