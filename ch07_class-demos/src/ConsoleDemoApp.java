7d7d5ccb94df601c5cfb8f8190d4565c4ab084d5
Sean Blessing <snblessing@gmail.com>
2021-06-08 16:35:39 -0400

    added print summary of a product

--- ch07_class-demos/src/ConsoleDemoApp.java
+++ ch07_class-demos/src/ConsoleDemoApp.java
@@ -2,12 +2,18 @@
 public class ConsoleDemoApp {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the Console Demo App!");
+		
+		
 		String code = Console.getString("Code?");
 		String desc = Console.getString("Description?");
 		double price = Console.getDouble("Price?", 0, 100000);
 		
 		Product p = new Product(code, desc, price);
 		
+		System.out.println("Product Summary:");
+		System.out.println(p);
 
 	}
 
