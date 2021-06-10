d1360d5ec3dd9638d7f6c929a7194a5028365615
Sean Blessing <snblessing@gmail.com>
2021-06-10 11:18:31 -0400

    ch9 class demos

---
+++ ch09_class-demos/src/InterfaceApp.java
@@ -0,0 +1,24 @@
+
+public class InterfaceApp {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Book b1 = new Book("java", "Murach's Java Programming", 
+							57.5, "Joel Murach");
+		
+		System.out.println(b1); // calls toString
+		printIt(b1);
+		
+		//cloneable - p. 316
+		Book b2 = (Book)b1.clone();
+		
+		System.out.println("B2:");
+		printIt(b2);
+		
+
+	}
+	
+	private static void printIt(Printable p) {
+		p.print();
+	}
+
+}
